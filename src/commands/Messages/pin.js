6c57a0b0ab437f8a972b9bfaca2fb03c0b01d8d1
KunoichiZ <KunoichiZ@users.noreply.github.com>
2020-05-21 14:41:30 -0500

    Create pin.js

---
+++ src/commands/Messages/pin.js
@@ -0,0 +1,20 @@
+const { Command } = require('klasa');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class extends Command {
+
+    constructor(...args) {
+        super(...args, { 
+            description: 'Pins the selected message to the channel\'s pinned messages.',
+            usage: "<message:string>"
+        });
+    }
+
+    run(msg, [message]) {
+        msg.channel.messages.fetch(message)
+            .then(result => {
+                result.pin();
+                msg.send(`Pinned message ID \`${message}\` to this channel.`)
+            });
+    }
+};
