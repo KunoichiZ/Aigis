ccc53afd3b9ff65f8969de4a9f7e5eafa2ed243f
KunoichiZ <KunoichiZ@users.noreply.github.com>
2020-05-21 14:52:46 -0500

    Create delete.js

---
+++ src/commands/Messages/delete.js
@@ -0,0 +1,19 @@
+const { Command } = require('klasa');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class extends Command {
+
+    constructor(...args) {
+        super(...args, { 
+            aliases: ['del'],
+            description: 'Deletes the message',
+            usage: "<amount:integer>"
+        });
+    }
+
+    run(msg, [amount]) {
+        msg.channel.bulkDelete(amount + 1)
+            .then(messages => msg.send(`Bulk deleted ${messages.size} messages`))
+            .catch(console.error);
+    }
+};
