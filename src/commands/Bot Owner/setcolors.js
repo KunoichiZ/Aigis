40a73ffaee566baef87b5454bc9fd8c7cbf3afa6
Kaoru <KunoichiZ@users.noreply.github.com>
2020-05-23 00:38:10 -0500

    Create setcolors.js

---
+++ src/commands/Bot Owner/setcolors.js
@@ -0,0 +1,16 @@
+const { Command } = require('klasa');
+
+module.exports = class extends Command {
+
+    constructor(...args) {
+        super(...args, { 
+            description: 'Sets the colored roles for the guild'
+        });
+    }
+
+    run(msg) {
+        let colors = ["Dark Hour Clock Blue", "Aigis' Hair", "Aigis' Eyes", "Sweet Indigo", "Cyan", "Pink", "haha gae", "P5 Red", "Potato Gold", "ShrinePink", "Fuuka's Hair", "Invisible", "P1 Purple", "Peachy", "dark red", "Velvet Room Blue"]
+        console.log(msg.guild.settings.set('colors', colors))
+    }
+    
+};
