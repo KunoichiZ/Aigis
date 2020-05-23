17d3b54f0a62e74fec374cf662dd1cb8afd73a59
Kaoru <KunoichiZ@users.noreply.github.com>
2020-05-23 00:39:17 -0500

    Create show.js

---
+++ src/commands/Roles/show.js
@@ -0,0 +1,21 @@
+const { Command } = require('klasa');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class extends Command {
+
+    constructor(...args) {
+        super(...args, { 
+            description: 'Lists all color roles that you can assign to yourself'
+        });
+    }
+
+    run(msg) {
+        let colors = msg.guild.settings.get('colors')
+        let colorEmbed = new MessageEmbed()
+            .setTitle('Color Roles')
+            .setColor(msg.member.displayHexColor)
+            .setDescription(colors.join(", "))
+        msg.send(colorEmbed)
+    }
+    
+};
