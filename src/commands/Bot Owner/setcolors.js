3b23627b565fb8bdbc159da52fa252cf50636488
Kaoru <KunoichiZ@users.noreply.github.com>
2020-05-23 00:53:05 -0500

    Update setcolors.js

--- src/commands/Bot Owner/setcolors.js
+++ src/commands/Bot Owner/setcolors.js
@@ -10,6 +10,7 @@ module.exports = class extends Command {
 
     run(msg) {
         let colors = ["Dark Hour Clock Blue", "Aigis' Hair", "Aigis' Eyes", "Sweet Indigo", "Cyan", "Pink", "haha gae", "P5 Red", "Potato Gold", "ShrinePink", "Fuuka's Hair", "Invisible", "P1 Purple", "Peachy", "dark red", "Velvet Room Blue"]
+        msg.guild.settings.set('colors', colors)
     }
     
 };
