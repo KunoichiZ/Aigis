bcbec523770fe9d7e670b15b41154290aa9b6a10
Kaoru <KunoichiZ@users.noreply.github.com>
2020-05-23 00:53:36 -0500

    Update getid.js

--- src/commands/Roles/getid.js
+++ src/commands/Roles/getid.js
@@ -5,12 +5,12 @@ module.exports = class extends Command {
     constructor(...args) {
         super(...args, { 
             description: 'Get the ID of a role without pinging it',
-            usage: '(rolename:string)'
+            usage: '(rolename:string)',
+            extendedHelp: 'This is the same as Aigis\' old \`get id` command'
         });
     }
 
     run(msg, [rolename]) {
-        
         let myRole = msg.guild.roles.find(role => role.name === rolename);
         if(myRole != undefined) {
             msg.send(`The ID for the role \`${rolename}\` is \`${myRole.id}\`.`)
