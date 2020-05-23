c25dea8ca5c21aca97eacbd117f0e20da0be100d
Kaoru <KunoichiZ@users.noreply.github.com>
2020-05-23 00:12:55 -0500

    Create give.js

---
+++ src/commands/Roles/give.js
@@ -0,0 +1,27 @@
+const { Command } = require('klasa');
+
+module.exports = class extends Command {
+
+    constructor(...args) {
+        super(...args, { 
+            description: 'Assigns yourself a role with a specific color',
+            usage: '(rolename:string)'
+        });
+    }
+
+    run(msg, [rolename]) {
+        
+        let myRole = msg.guild.roles.find(role => role.name === rolename);
+        if(myRole != undefined) {
+            if(msg.member.roles.has(myRole.id)) {
+                msg.channel.send(`You already have the ${rolename} role!`);
+             } else {
+                 msg.member.roles.add(myRole).catch(console.error);
+                 msg.channel.send(`You've been given the ${rolename} role!`);
+             }
+        } else {
+            msg.send("That role doesn't exist!")
+        }
+    }
+    
+};
