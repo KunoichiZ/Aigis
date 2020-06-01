00ca3de4db55c06366b72dcfbbd512e2409aacf8
Kaoru <KunoichiZ@users.noreply.github.com>
2020-06-01 12:52:27 -0500

    Create rename.js

---
+++ src/commands/Roles/rename.js
@@ -0,0 +1,24 @@
+const { Command } = require('klasa');
+
+module.exports = class extends Command {
+
+    constructor(...args) {
+        super(...args, { 
+            description: 'Change an existing color role\'s name.',
+            usage: '(rolename:string) (newname:string)',
+            usageDelim: ',',
+            extendedHelp: 'This is the same as Aigis\' old \`rename color` command'
+        });
+    }
+
+    run(msg, [rolename, newname]) {
+        let myRole = msg.guild.roles.find(role => role.name === rolename);
+        if(myRole != undefined) {
+            myRole.edit({ name: newname })
+            .then(msg.send(`Edited role \`${rolename}\` name to \`${newname}\``))
+        } else {
+            msg.send("That role doesn't exist!")
+        }
+    }
+    
+};
